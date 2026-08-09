13ae4209e57c9a26f7527cd86aef0ef4b6493989
mariamelsayed12 <me282573@gmail.com>
2026-08-09 22:18:50 +0300

    use layout effect for synchronous scroll restoration to eliminate layout transition paint lag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
 const HomePage = () => {
   const location = useLocation();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
